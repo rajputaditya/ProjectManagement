1fb12683ccf7ba7cca2b81bb8d8db53b25b69db7
b7ibmjava02@iiht.tech <b7ibmjava02@iiht.tech>
2019-11-18 10:55:34 +0530

    MERGER

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,79 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-<<<<<<< HEAD:src/app/tasks/tasks.component.ts
-import { TaskServiceService } from './task-service.service';
-=======
 import { HttpClient } from '@angular/common/http';
 import { CommonService } from '../common.service';
 
->>>>>>> origin/master:src/app/project-details/project-details.component.ts
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.scss']
 })
-<<<<<<< HEAD:src/app/tasks/tasks.component.ts
-
-//My Class
-export class TaskClass{
-  projectTitle:string;
-  taskTitle:string;
-  taskOwner:string;
-  taskDescription;
-  endDateMyCLass;
-  startDateMyClass;
-
-  setProjectTitle(projectTitle){
-    this.projectTitle=projectTitle;
-  }
-  setTasktitle(taskTitle){
-    this.taskTitle=taskTitle;
-  }
-  setTaskOwner(taskOwner){
-    this.taskOwner=taskOwner;
-  }
-  setTaskDescription(taskDescription){
-    this.taskDescription=taskDescription;
-  }
-  setStartDate(startDate){
-    this.startDateMyClass=startDate;
-  }
-  setEndDate(endDate){
-    this.endDateMyCLass=endDate;
-  }
-}
-
-export class TasksComponent implements OnInit {
-=======
 export class ProjectDetailsComponent implements OnInit {
->>>>>>> origin/master:src/app/project-details/project-details.component.ts
 
   firstnamebind: string;
   taskbind: any = "Manager";
   buttons = Array().fill(false);
   newTaskForm: FormGroup;
   editTaskForm: FormGroup;
-<<<<<<< HEAD:src/app/tasks/tasks.component.ts
-=======
 
   endDate;
   startDate;
->>>>>>> origin/master:src/app/project-details/project-details.component.ts
   temp;
   editendDate;
   editstartDate;
   endtemp;
-<<<<<<< HEAD:src/app/tasks/tasks.component.ts
-  endDate;
-  startDate;
-  taskClass:TaskClass;
-
-  
-=======
   projectDetail;
 
->>>>>>> origin/master:src/app/project-details/project-details.component.ts
 
 
   get getProjectTitle() {
@@ -125,11 +76,7 @@ export class ProjectDetailsComponent implements OnInit {
   userArray: Array<any> = [];
   url: string = "https://jsonplaceholder.typicode.com/users";
 
-<<<<<<< HEAD:src/app/tasks/tasks.component.ts
-  constructor(private http: HttpClient,private service:TaskServiceService) {
-=======
   constructor(private http: HttpClient, private comServ: CommonService) {
->>>>>>> origin/master:src/app/project-details/project-details.component.ts
     this.http.get(this.url).subscribe(data => {
       JSON.parse(JSON.stringify(data)).forEach(element => {
         this.userArray.push(element);
@@ -149,13 +96,9 @@ export class ProjectDetailsComponent implements OnInit {
     this.editTaskForm = new FormGroup({
       editTaskOwnerValidator: new FormControl('', [Validators.required, Validators.minLength(3)]),
       editTaskDescriptionValidator: new FormControl('', [Validators.required, Validators.minLength(20), Validators.maxLength(100)]),
-<<<<<<< HEAD:src/app/tasks/tasks.component.ts
-      editEndDateValidator:new FormControl('', [Validators.required])
-=======
       editEndDateValidator: new FormControl('', [Validators.required])
 
 
->>>>>>> origin/master:src/app/project-details/project-details.component.ts
     });
 
     this.newTaskForm = new FormGroup({
@@ -180,21 +123,5 @@ export class ProjectDetailsComponent implements OnInit {
     console.log(this.curId);
   }
 
-<<<<<<< HEAD:src/app/tasks/tasks.component.ts
-  //Adding New Task
-
-  addNewTask(){
-
-    this.taskClass=new TaskClass();
-    this.taskClass.setProjectTitle((<HTMLInputElement>document.getElementById("projectTitle")).value);
-    this.taskClass.setTasktitle((<HTMLInputElement>document.getElementById("taskTitle")).value);
-    this.taskClass.setTaskOwner((<HTMLInputElement>document.getElementById("taskOwner")).value);
-    this.taskClass.setTaskDescription((<HTMLInputElement>document.getElementById("taskDescription")).value);
-    this.taskClass.setStartDate((<HTMLInputElement>document.getElementById("projectStartdate")).value);
-    this.taskClass.setEndDate((<HTMLInputElement>document.getElementById("projectEnddate")).value);
-    this.service.saveTask(this.taskClass);
-  }
-=======
->>>>>>> origin/master:src/app/project-details/project-details.component.ts
 
 }
