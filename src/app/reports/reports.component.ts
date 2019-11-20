ab14ca8d78ef258d6d23faa67585e73b676b400e
Sharvinder Kour <sharkou1@in.ibm.com>
2019-11-20 10:01:12 +0530

    Intermediate Commit

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -66,58 +66,58 @@ export class ReportsComponent implements OnInit {
             }
           }
         });
+        let smDTS = [{
+          label: "Akhil",
+          data: [1,3,76,23,76,21,87],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+
+          fill: true
+        },
+        {
+          data: [32,76,45,98,34,1,55],
+
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          fill: true
+        }];
         this.chart = new Chart('canvas', {
-          type: 'bar',
+          type: 'line',
           animationEnabled: true,
           axisX: {
             intervsl: 10,
           },
           data: {
-            labels: [],
-            datasets: [
-              {
-                data: [],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-                ],
-
-                fill: true
-              },
-              {
-                data: [],
-
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-                ],
-                fill: true
-              },
-            ]
+            labels: ["Janaury","Febuary","March","April","May","June","July","August","September","October","November","December"],
+            datasets: smDTS
           },
           options: {
             legend: {
@@ -128,9 +128,10 @@ export class ReportsComponent implements OnInit {
 
                 display: true
               }],
-              yAxes: [{
-                display: true
-              }],
+              yAxes: [
+                {
+                  "ticks": { "beginAtZero": true }
+                }],
             }
           }
         });
