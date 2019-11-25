cd5f699a8dcaf93aa322ae63ac983286ea5d3a4b
Jaspreet Bhatti <altairximate@gmail.com>
2019-11-25 16:53:00 +0530

    Added Upcoming to Navbar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { formatDate } from '@fullcalendar/core';
+import * as moment from 'moment'
+import { GetEventsService } from '../calendar/get-events.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,25 +11,76 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NavBarComponent implements OnInit {
 
-  usersArray: Array<any>=[];
-// searchText:String;
-url:string;
+  usersArray: Array<any> = [];
+  // searchText:String;
+  url: string;
+  myEvents: any = [];
 
-set(){
-   this.usersArray=[];
-  this.url='http://localhost:8080/search/'+(<HTMLInputElement>document.getElementById("searchBox")).value;
-  console.log(this.url);
-  this.http.get(this.url).subscribe(data => {
-    // Populating usersArray with names from API
-    JSON.parse(JSON.stringify(data)).forEach(element => {
-      this.usersArray.push(element);
+  set() {
+    this.usersArray = [];
+    this.url = 'http://localhost:8080/search/' + (<HTMLInputElement>document.getElementById("searchBox")).value;
+    console.log(this.url);
+    this.http.get(this.url).subscribe(data => {
+      // Populating usersArray with names from API
+      JSON.parse(JSON.stringify(data)).forEach(element => {
+        this.usersArray.push(element);
+      });
     });
-  });
-}
-constructor(private http: HttpClient) {
- 
-}
+  }
+
+  getUpcoming() {
+    this.getEvents();
+    document.getElementById("upcomingAlert").hidden = !document.getElementById("upcomingAlert").hidden;
+    setTimeout(() => {
+      document.getElementById("upcomingAlert").hidden = !document.getElementById("upcomingAlert").hidden;
+    }, 3000);
+  }
+
+  getEvents(){
+    let subscription = this.getService.getEvents().subscribe(
+      (events) => {
+        this.myEvents = events;
+        this.getReminder();
+      });
+  }
+
+  getReminder() {
+    let crEvt = { title: '', start: moment().add(100, 'y').toISOString() };
+    let crTM = moment().toISOString();
+    let check = crEvt.start;
+    this.myEvents.forEach(event => {
+      if (moment(event.start, moment.ISO_8601).isValid()) {
+        if ((moment(event.start, moment.ISO_8601).isAfter(moment(crTM, moment.ISO_8601)))) {
+          if (moment(event.start, moment.ISO_8601).isBefore(moment(crEvt.start, moment.ISO_8601))) {
+            crEvt = event;
+          }
+        }
+      }
+    });
+    if (crEvt.start != check) {
+      document.getElementById('notification').innerHTML =
+        "<b>Title: </b>"
+        + crEvt.title
+        + "<br><b>Date: </b>"
+        + formatDate(crEvt.start, {
+          month: 'long',
+          year: 'numeric',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          timezone: 'IST'
+        });
+    } else {
+      document.getElementById('notification').innerHTML = "<b>No Upcoming Event<b>"
+    }
+  }
+
+
+  constructor(private http: HttpClient, private getService: GetEventsService) {
+
+  }
   ngOnInit() {
+    this.getEvents();
   }
 
 }
